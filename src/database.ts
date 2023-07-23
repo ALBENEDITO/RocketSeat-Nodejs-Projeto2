25a9c5428c08cab7e152d74979aa63455a5f4b15
Allan Luiz Benedito <allan.benedito@gmail.com>
2023-07-22 23:05:41 -0300

    deploy ajustes para render

--- src/database.ts
+++ src/database.ts
@@ -3,10 +3,13 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: 'sqlite',
-  connection: {
-    filename: env.DATABASE_URL, // O CAMINHO DO BANCO ESTÁ NO DOTENV
-  },
+  client: env.DATABASE_CLIENT,
+  connection:
+    env.DATABASE_CLIENT === 'sqlite'
+      ? {
+          filename: env.DATABASE_URL, // O CAMINHO DO BANCO ESTÁ NO DOTENV
+        }
+      : env.DATABASE_URL,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
